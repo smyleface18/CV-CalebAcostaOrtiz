f7513c80947bda8f3802710644fa82bdbb4c8626
Caleb <ca30850@gmail.com>
2024-10-06 17:08:05 -0500

    updata card project

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,7 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true, 
     autoplay: {
-      delay: 1000
+      delay: 380
     },
     // And if we need scrollbar
     scrollbar: {
@@ -15,14 +15,16 @@ const swiper = new Swiper('.swiper', {
   });
 
 
-  const head_project =[...document.querySelectorAll("#proyectos")];
+  const head_project =[...document.querySelectorAll("#head_project")];
   const body_project = [...document.querySelectorAll(".body_project")];
-
+  const iconProject = [...document.querySelectorAll("#iconProject")];
   for (let index = 0; index < head_project.length; index++) {
     let head = head_project[index];
     let body = body_project[index];
+    let icon = iconProject[index];
       head.addEventListener("click", ()=>{
           body.classList.toggle("none")
+          icon.classList.toggle("questions_arrow--rotate")
       });
 
     }
