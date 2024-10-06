0e125872c2c015cb3524de4c9fe00e887ea9beee
Caleb <ca30850@gmail.com>
2024-10-05 23:41:22 -0500

    finish refactor cards

--- js/functions.js
+++ js/functions.js
@@ -4,15 +4,10 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-    loop: true,
-  
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    loop: true, 
+    autoplay: {
+      delay: 1000
     },
-  
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
